94a789f2455ce016a6202f1a1fef0585ea1dd4a0
Bo Lopker <lopkerk@gmail.com>
2026-03-28 10:21:37 -0700

    Release v0.1.5

--- packages/degenerate/bin/degenerate.dart
+++ packages/degenerate/bin/degenerate.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:args/args.dart';
 import 'package:degenerate/src/generator.dart';
 
-const String version = '0.1.4';
+const String version = '0.1.5';
 
 ArgParser buildParser() {
   return ArgParser()
